a91576679e9afa89ebe2a2e4c79bbdfd77fb1cfc
Samuel <sammorris207@gmail.com>
2024-09-29 22:28:30 +0530

    app name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-16-scss-two'`, () => {
+  it(`should have as title 'rti-employer-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-16-scss-two');
+    expect(app.title).toEqual('rti-employer-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-16-scss-two app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rti-employer-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-16-scss-two';
+  title = 'rti-employer-app';
   
 }
